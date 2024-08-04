f47c3832897fb1cc3325daa7d59fd6e2ca3d7fa7
Harsh_Aakoliya <harshaakoliya20@gmail.com>
2024-08-05 01:08:17 +0530

    a;skfdj

--- index.js
+++ index.js
@@ -10,15 +10,9 @@ require("dotenv").config()
 
 app.use(cors());
 
-const pool=new Pool({
-    // user:process.env.USER_NAME,
-    // host:process.env.HOST_NAME,
-    // database:process.env.DB_NAME,
-    // password:process.env.DB_PASSWORD,
-    // dialect:process.env.DB_DIALECT,
-    // port:process.env.PORT_NUMBER,
-    connectionString: process.env.POSTGRES_URL,
-});
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
 
 pool.connect((err,client,release)=>{
     if(err) return console.error("Error in connection");
